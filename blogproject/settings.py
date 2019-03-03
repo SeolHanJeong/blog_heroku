74bd1ef858d53d43644c5cce1ec39670e236afaf
hanjeong <dkfzpdls@naver.com>
2019-03-03 15:13:10 +0900

    allow modify

--- blogproject/settings.py
+++ blogproject/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY =os.environ.get('DJANGO_SECRET_KEY', '!96r3s@5b@*h^yhcn=wdvqev=_n+5x5
 # DEBUG = True
 DEBUG = bool(os.environ.get('DJANGO_DEBUG',True))
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
